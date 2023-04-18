7ce651a621caff61086539aa31deccd9eb839187
Yesong Choi <to06109@naver.com>
2023-04-18 23:05:36 +0900

    refactor: 코드 리팩토링
    - if-else문 대신 switch문 사용
    - 상수 이용

--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ const number = document.querySelector('span');
 
 number.innerText = '0';
 
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') return count + 1;
-  else if (action.type === 'MINUS') return count - 1;
-  return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
 const countStore = createStore(countModifier);
@@ -22,11 +30,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: 'ADD' });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: 'MINUS' });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener('click', handleAdd);
