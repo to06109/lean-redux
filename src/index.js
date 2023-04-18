e310555e297adc2e4cccdbe011f793926d185641
Yesong Choi <to06109@naver.com>
2023-04-18 22:43:56 +0900

    feat: store, reducer, action

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-number.innerText = count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
 
-const updateText = () => {
-  number.innerText = count;
+  if (action.type === 'ADD') return count + 1;
+  else if (action.type === 'MINUS') return count - 1;
+  return count;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState()); //5
